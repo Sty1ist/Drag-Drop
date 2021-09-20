a14b13eb14c48b99087741d2634b106c9b41825a
Ins <imbalancerm@gmail.com>
2021-09-21 00:11:13 +0300

    написал  script.js

---
+++ script.js
@@ -0,0 +1,39 @@
+const item = document.querySelector('.item')
+const placeholders = document.querySelectorAll('.placeholder')
+
+item.addEventListener('dragstart', dragstart)
+item.addEventListener('dragend', dragend)
+
+for (const placeholder of placeholders) {
+    placeholder.addEventListener('dragover')
+    placeholder.addEventListener('dragenter')
+    placeholder.addEventListener('dragleave')
+    placeholder.addEventListener('drop')
+
+}
+
+function dragstart(event) {
+    event.target.classList.add('hold')
+    setTimeout(() => event.target.classList.add('hide'), 0)
+}
+
+function dragend(event) {
+    event.target.className = 'item'
+}
+
+function dragover(event) {
+    event.preventDefault()
+}
+
+
+function dragenter(event) {
+    event.target.classList.add('hovered')
+}
+
+function dragleave(event) {
+    event.target.classList.remove('hovered')
+}
+
+function dragdrop(event) {
+    event.target.append(item)
+}
